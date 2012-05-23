1d559a2081f18eb5073ee22adc6b4cd3cda75e4b
RodrigoJimmy <rodrigodelimavieira@gmail.com>
2012-05-23 13:23:55 +0000

    Usuário alterado: ubuntu -> rodrigo
    Para facilitar o uso de login por par de chaves no ssh, criei um usuário no servidor com o mesmo que eu utilizo na minha estação de trabalho.
    Migrei o projeto para o /home do novo usuário
    Altrei as configurações do banco de dados para refletir a nova localização do arquivo uniwiki.db

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'firebird', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/ubuntu/uniwiki/db/uniwiki.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/rodrigo/uniwiki/db/uniwiki.db',                      # Or path to database file if using sqlite3.
         'USER': 'SYSDBA',                      # Not used with sqlite3.
         'PASSWORD': '123456',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
