ee8940badcaaefc244e4946caa1d1e65a12ffd11
RodrigoJimmy <rodrigodelimavieira@gmail.com>
2012-05-24 13:54:40 +0000

    Modelo para Notícias criado Criado "News" Model para armazenar as notícias App cadastrada em INSTALLED_APPS no settings.py

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+	pub_date = models.DateTimeField()
+	headline = models.CharField(max_length=200)
+	content  = models.TextField()
+	photo    = models.ImageField(upload_to='/photos/')
+
+	class Meta:
+		ordering = ["-pub_date"]
+		verbose_name_plural = "news"
+
+	def __unicode__(self):
+		return self.headline
